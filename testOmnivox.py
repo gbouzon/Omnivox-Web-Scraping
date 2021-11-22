052d1144f9625933a66c1017714443670e7b3b8c
Giuliana Bouzon <ga@bouzon.com.br>
2021-11-22 02:04:32 -0500

    modified omnivox session to fetch vanier data

--- testOmnivox.py
+++ testOmnivox.py
@@ -4,7 +4,7 @@ from OmnivoxSession import OmnivoxSession
 async def main():
     username = input("What is your Omnivox username: ")
     password = input("What is your Omnivox password: ")
-    session = OmnivoxSession("jac", username, password)
+    session = OmnivoxSession("vc", username, password)
     session = await session.login()
     if not session:
         return print('Login failed')
